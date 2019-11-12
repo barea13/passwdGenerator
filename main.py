43e13503a3e85652720eef0bc9a6f1bac45df4c8
Jaime Barea <barea1396@gmail.com>
2019-11-12 12:48:12 +0100

    Added default function

--- main.py
+++ main.py
@@ -3,19 +3,16 @@ import os
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
+def default(v, i):
+    if v == '':
+        v = i
+    else:
+        v = int(v)
+
 quant = input('Introduce the quantity of passwords (default 4): ')
-
-if quant == '':
-    quant = 4
-else:
-    quant = int(quant)
-
+default(quant, 4)
 length = input('Introduce the lenght of your password (default 10): ')
-
-if length == '':
-    length = 10
-else:
-    length = int(length)
+default(length, 10)
 
 for q in range(quant):
     password = ''
