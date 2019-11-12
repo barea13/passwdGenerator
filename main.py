fd3908c821d406937a52e50b5fdb54e2b9bc0919
Jaime Barea <barea1396@gmail.com>
2019-11-12 12:29:33 +0100

    Added quantity of password

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import random
+import os
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
-length = input('Introduce the lenght of your password: ')
-length = int(length)
-password = ''
+quant = input('Introduce the quantity of passwords (default 4): ')
 
-for c in range(length):
-    password += random.choice(chars)
-print(password)
+if quant == '':
+    quant = 4
+else:
+    quant = int(quant)
+
+length = input('Introduce the lenght of your password (default 10): ')
+
+if length == '':
+    length = 10
+else:
+    length = int(length)
+
+for q in range(quant):
+    password = ''
+    for c in range(length):
+        password += random.choice(chars)
+    print(password)
